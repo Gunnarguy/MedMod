3e2aa841a8caa591ea69b35f5eea6e76ebd5472d
gunnarguy <gunnarguy@me.com>
2026-03-20 17:44:16 -0700

    Initial Commit

---
+++ MedMod/MedModApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedModApp.swift
+//  MedMod
+//
+//  Created by Gunnar Hostetler on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct MedModApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
